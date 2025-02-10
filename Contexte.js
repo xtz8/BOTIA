e50fc1642e3cd5dff3641e95a627545453c32fd6
xtz8 <163120277+xtz8@users.noreply.github.com>
2025-02-10 22:47:45 +0100

    Create Contexte.js

---
+++ Contexte.js
@@ -0,0 +1,72 @@
+const contexteCV = `
+Je m'appelle Lamia AMMI-ALI, et j'ai 13 ans d'expérience en qualité logicielle, dont 5 ans en tant que freelance. Je suis actuellement en formation en cybersécurité chez FITEC à Nanterre, un domaine qui me passionne. J'ai une grande autonomie et une capacité d'adaptation rapide, et je place la satisfaction client au cœur de mes priorités. Je suis ouverte aux idées des autres et je m'appuie sur le travail d'équipe pour trouver les meilleures solutions.
+
+**Expériences professionnelles :**
+
+1. **Formation en Cybersécurité** (03/2024 - 06/2024) - FITEC, Nanterre :
+   - Formation de 400 heures en cybersécurité.
+   - Compétences acquises : Évaluation des risques de sécurité (ISO 27005, EBIOS RM), mise en place d'un SMSI (ISO 27001), durcissement Linux/Windows, UTM Stormshield, SOC, Pentest, OSINT.
+   - Réalisation clé : Préparation d'une soutenance sur la sécurité informatique d'une entreprise fabricant des vaccins, avec analyse de l'architecture existante et proposition d'une nouvelle architecture conforme aux recommandations de l'ANSSI.
+
+2. **Assurance qualité Senior** (04/2023 - 11/2023) - LUNII, Paris :
+   - Projet : Sortie d'une fabrique à histoire connectée.
+   - Responsabilités : Implémentation des bonnes pratiques QA, reporting d'avancement des tests, mise en place de la stratégie de recette, rédaction des plans de tests, création et suivi des anomalies.
+   - Réalisation clé : Mise en production applicative sans anomalies, permettant le lancement de la production de milliers d'exemplaires en usine dans les délais.
+
+3. **Test Lead/Stream Lead** (02/2022 - 02/2023) - Disneyland Paris, Marne-la-Vallée :
+   - Projets : Migration de TPE (Ingenico/Nepting), mise en place de bornes automatiques dans les parkings Disney.
+   - Responsabilités : Conduite des UAT, organisation des comités de suivi d'anomalies, rédaction des plans de tests, reporting d'avancement des tests.
+   - Réalisation clé : Détection proactive d'un problème matériel sur un site pilote, garantissant un déploiement sans incidents.
+
+4. **Assurance qualité Senior** (11/2018 - 12/2021) - Groupe CAT, Suresnes :
+   - Projet : Évolution des applications de planification de transport.
+   - Responsabilités : Mise en place de la stratégie de recette, rédaction des plans de tests, tests de non-régression.
+   - Réalisation clé : Mise en place d'une approche de tests en BDD, améliorant la qualité des développements et réduisant les anomalies.
+
+5. **Auditrice assurance qualité** (09/2017 - 11/2017) - TF1, Paris :
+   - Projet : Audit des pratiques de tests et recommandations pour améliorer la qualité.
+   - Responsabilités : Choix d'outils de tests, formation à Squash TM, implémentation d'un plan d'assurance qualité.
+
+6. **Assurance qualité Senior** (09/2016 - 06/2017) - OYST, Paris :
+   - Projet : Solution de paiement en ligne en un clic.
+   - Responsabilités : Mise en place de la stratégie de recette, choix d'un référentiel de tests, rédaction des plans de tests.
+
+7. **Assurance qualité** (09/2014 - 06/2015) - SOFTFLUENT (COLAS), Antony :
+   - Projet : Outil de gestion de consommation de temps et de fournitures pour COLAS.
+   - Environnement : Agile, TFS, Webservice, XML, SoapUI.
+
+8. **Assurance qualité** (01/2014 - 06/2014) - CETELEM, Levallois :
+   - Projet : Refonte des parcours de souscription et simulation de prêts.
+   - Environnement : Agile, QC-ALM, Webservice, XML, WCM (Autonomy), SoapUI.
+
+9. **Assurance qualité** (03/2012 - 04/2013) - Samsung Electronique France, Villepinte :
+   - Projet : Ajout de KPI pour SAP.
+   - Environnement : XML, Excel, Webservice, SAP.
+
+10. **Assurance qualité** (03/2011 - 02/2012) - Agence Nationale pour la Rénovation Urbaine, Paris :
+    - Projet : Recette des évolutions fonctionnelles.
+    - Environnement : OTRS, Excel, Web.
+
+11. **Assurance qualité** (03/2010 - 02/2011) - NATIXIS Financement, Paris :
+    - Projet : Instruction de crédits à la consommation pour la Banque Populaire et la Caisse d'Épargne.
+    - Environnement : Quality Center, Méthode Agile, Excel, Web, XML, Webservice.
+
+**Compétences techniques :**
+- Linux, HardeningKitty, Kali, UTM Stormshield, Jira, Postman, QC-ALM, Prestashop, Pack Office.
+
+**Compétences métier :**
+- Analyse et gestion de risques (ISO 27005, EBIOS RM), méthodologie Agile.
+
+**Formations :**
+- **Certifié EBIOS RM** (PECB, 06/2024).
+- **Analyste Cybersécurité** (FITEC, RNCP-7, 06/2021 - 10/2021).
+- **Analyse technique des marchés financiers** (ESLSCA Business School, RNCP-7).
+- **Stage Méthodes Agiles, Product Owner** (M2i Formation, 01/2016).
+- **Licence en administration publique** (ISMAPP, Paris, 09/2007 - 09/2008).
+
+**Langues :**
+- Français (langue maternelle), Anglais (niveau B2).
+
+**Secteurs d'activité :**
+- Banque, marchés financiers, paiement, e-commerce, urbanisme, BTP, IoT, transport et logistique.
+`;
